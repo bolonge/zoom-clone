a9533e9a9a2937dcee2b26374a108d5a02b721d3
bolonge <qhsghd5357@gmail.com>
2022-08-20 21:51:56 +0900

    add:build를 삭제하기위해 del 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,11 @@
 import gulp from "gulp";
+import del from "del";
+
+const routes = {
+  src: "src/*.js",
+  build: "build",
+};
+
+const clean = () => del(["build"]);
+
+export const dev = gulp.series([clean]);
